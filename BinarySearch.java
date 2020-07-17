2d3e72debaa9308c99872edce7b7924abc497edb
sudhanshusingh8570 <40402301+sudhanshusingh8570@users.noreply.github.com>
2020-07-17 10:29:38 +0530

    codes

--- BinarySearch.java
+++ BinarySearch.java
@@ -5,31 +5,31 @@ import java.util.*;
 public class BinarySearch {
 
 	// returns index where element found
-	
-	
+	// running time complexity: O(log n)
+
 	// Recursive implementation
 	private static int binarySearchRec(int a[], int low, int high, int key) {
 		if(low>high)return -1;  // element not found
-		
+
 		int mid=(low+high)/2;
-		
+
 		if(a[mid]==key)return mid;
 		else if (a[mid]>key) return binarySearchRec(a, low, mid-1, key);
 		return binarySearchRec(a, mid+1, high, key);
 	}
-	
-	
+
+
 	// Iterative implementation
 	private static int binarySearchItr(int a[],int low, int high, int key) {
 		while(low<=high) {
 			int mid=(low+high)/2;
 			if(a[mid]==key) return mid;
 			else if(a[mid]>key) high=mid-1;
-			else low=mid+1;	
+			else low=mid+1;
 		}
 		return -1;
 	}
-	
+
 	// Method to print Array
 	private static void printArray(int a[]) {
 		for(int i=0;i<a.length;i++) {
@@ -37,20 +37,20 @@ public class BinarySearch {
 		}
 		System.out.println();
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		int a[]= {3,4,6,8,9,1,0,11,14};
 		Arrays.sort(a);
-		
+
 		printArray(a);
 		System.out.println("Search 11: "+binarySearchRec(a, 0, a.length-1, 11));
 		System.out.println("Search 11: "+binarySearchItr(a, 0, a.length-1, 11));
 		System.out.println("Search 11: "+Arrays.binarySearch(a, 11));
-		
+
 		System.out.println("Search 100: "+binarySearchRec(a, 0, a.length-1, 100));
 		System.out.println("Search 100: "+binarySearchItr(a, 0, a.length-1, 100));
 		System.out.println("Search 100: "+Arrays.binarySearch(a, 100));
-		
+
 	}
 
 }
---
+++ PrimeNumber.java
@@ -0,0 +1,24 @@
+package fundamental;
+
+public class PrimeNumber {
+	private static boolean isPrime(int n) {
+		if(n<2)return false;
+		for(int i=2;i*i<=n;i++) {
+			if(n%i==0) return false;
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		int x=22;
+		System.out.println(x+" isPrime: "+isPrime(x));
+		
+		x=43;
+		System.out.println(x+" isPrime: "+isPrime(x));
+		
+		String xx=Integer.toString(x);
+		System.out.println(xx+"hey there");
+		System.out.println(Math.PI);
+		System.out.printf("%.2f", Math.PI);
+	}
+
+}
